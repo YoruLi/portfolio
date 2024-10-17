dbe327b10bc1e0f202f96fddba4dff15cabff5c7
Lionel Farias <Lionel3327@gmail.com>
2024-10-17 05:10:44 -0300

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -13,6 +12,6 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-  output: "server",
-  adapter: vercel(),
+  output: "static",
+
 });
