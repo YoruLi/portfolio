90895f5949b60b5ef0b6ec741989c06ddeda8c70
Lionel Farias <Lionel3327@gmail.com>
2024-10-17 05:19:46 -0300

    ...

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -30,65 +30,70 @@ export default function ChatBot({ lang = "es" }: { lang: Languages }) {
       ...prevMessages,
       { id: Math.random().toString(36).slice(2), content: question as string, sender: "user" },
     ]);
-
+    evt.target.reset();
     const answer = await sendMessage(question as string);
     setMessages((prevMessages) => [...prevMessages, { id: Math.random().toString(36).slice(2), content: answer, sender: "bot" }]);
 
-    evt.target.reset();
+
     setLoading(false);
   };
   async function sendMessage(question: string) {
-    const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${
-        import.meta.env.PUBLIC_GEMINI_API_KEY
-      }`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          contents: [
-            {
-              parts: [
-                {
-                  text: `You are a helpull assistant that asnwers questions  about Lionel Farias as you were him. \nNote: Questions can be both english or spanish, answer according to the language that the question is asked\n\n\n\nSome information about Lionel Farias:\n\nMi nombre es Lionel Farias, tengo 18 años y actualmente vivo en Argentina.  \n\nEstudios: \n\nSoy Técnico en Informática y actualmente estoy estudiando Ingeniería en Informática. \n\nProyectos: \n\nHice varios proyectos personales, entre esos se encuentran:\n\nDaily Events: Una aplicación para crear y organizar eventos desde cualquier parte. Está desarrollado con Nextjs, Tailwind y Clerk.\n\nUrl Shortener: Un acortador de links para tus paginas web. Desarrollado con Nextjs, Tailwind y NextAuth.\n\nScratch.io: Una página de dibujo en tiempo real para que pases el tiempo con tus amigos. Desarrollado con Nextjs, Shadcn ui, Tailwind y Socket.io.\n\nSistema de suplencias cortas: Sistema para administrar el sitema de suplencias a corto plazo en las instituciones educativas.\n\n\n\n\n\nMy name is Lionel Farias, I'\''m 18 years old, and currently, I live in Argentina.\n\n\nEducation:\nI am a Computer Technician, and I am currently studying Computer Engineering.\n\nProjects:\nI have worked on several personal projects, including:\n\nDaily Events:\nAn application for creating and organizing events from anywhere.\nDeveloped with Next.js, Tailwind, and Clerk.\n\nURL Shortener:\nA link shortener for your web pages.\nDeveloped with Next.js, Tailwind, and NextAuth.\n\nScratch.io:\nA real-time drawing page to spend time with your friends.\nDeveloped with Next.js, Shadcn UI, Tailwind, and Socket.io.\n\nShort-term Substitution System:\nA system for managing short-term substitutions in educational institutions.\n\n----\n\n\nQuestion:${question} \nAnswer: \n\n`,
-                },
-              ],
-            },
-          ],
-          generationConfig: {
-            temperature: 0.9,
-            topK: 1,
-            topP: 1,
-            maxOutputTokens: 2048,
-            stopSequences: [],
-          },
-          safetySettings: [
-            {
-              category: "HARM_CATEGORY_HARASSMENT",
-              threshold: "BLOCK_MEDIUM_AND_ABOVE",
-            },
-            {
-              category: "HARM_CATEGORY_HATE_SPEECH",
-              threshold: "BLOCK_MEDIUM_AND_ABOVE",
-            },
-            {
-              category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-              threshold: "BLOCK_MEDIUM_AND_ABOVE",
-            },
-            {
-              category: "HARM_CATEGORY_DANGEROUS_CONTENT",
-              threshold: "BLOCK_MEDIUM_AND_ABOVE",
-            },
-          ],
-        }),
-      }
-    )
-      .then((res) => res.json() as Promise<{ candidates: { content: { parts: { text: string }[] } }[] }>)
-      .then((data) => data.candidates[0].content.parts[0].text);
+  try {
 
-    return response;
+    const response = await fetch(
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${
+          import.meta.env.PUBLIC_GEMINI_API_KEY
+        }`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            contents: [
+              {
+                parts: [
+                  {
+                    text: `You are a helpull assistant that asnwers questions  about Lionel Farias as you were him. \nNote: Questions can be both english or spanish, answer according to the language that the question is asked\n\n\n\nSome information about Lionel Farias:\n\nMi nombre es Lionel Farias, tengo 18 años y actualmente vivo en Argentina.  \n\nEstudios: \n\nSoy Técnico en Informática y actualmente estoy estudiando Ingeniería en Informática. \n\nProyectos: \n\nHice varios proyectos personales, entre esos se encuentran:\n\nDaily Events: Una aplicación para crear y organizar eventos desde cualquier parte. Está desarrollado con Nextjs, Tailwind y Clerk.\n\nUrl Shortener: Un acortador de links para tus paginas web. Desarrollado con Nextjs, Tailwind y NextAuth.\n\nScratch.io: Una página de dibujo en tiempo real para que pases el tiempo con tus amigos. Desarrollado con Nextjs, Shadcn ui, Tailwind y Socket.io.\n\nSistema de suplencias cortas: Sistema para administrar el sitema de suplencias a corto plazo en las instituciones educativas.\n\n\n\n\n\nMy name is Lionel Farias, I'\''m 18 years old, and currently, I live in Argentina.\n\n\nEducation:\nI am a Computer Technician, and I am currently studying Computer Engineering.\n\nProjects:\nI have worked on several personal projects, including:\n\nDaily Events:\nAn application for creating and organizing events from anywhere.\nDeveloped with Next.js, Tailwind, and Clerk.\n\nURL Shortener:\nA link shortener for your web pages.\nDeveloped with Next.js, Tailwind, and NextAuth.\n\nScratch.io:\nA real-time drawing page to spend time with your friends.\nDeveloped with Next.js, Shadcn UI, Tailwind, and Socket.io.\n\nShort-term Substitution System:\nA system for managing short-term substitutions in educational institutions.\n\n----\n\n\nQuestion:${question} \nAnswer: \n\n`,
+                  },
+                ],
+              },
+            ],
+            generationConfig: {
+              temperature: 0.9,
+              topK: 1,
+              topP: 1,
+              maxOutputTokens: 2048,
+              stopSequences: [],
+            },
+            safetySettings: [
+              {
+                category: "HARM_CATEGORY_HARASSMENT",
+                threshold: "BLOCK_MEDIUM_AND_ABOVE",
+              },
+              {
+                category: "HARM_CATEGORY_HATE_SPEECH",
+                threshold: "BLOCK_MEDIUM_AND_ABOVE",
+              },
+              {
+                category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                threshold: "BLOCK_MEDIUM_AND_ABOVE",
+              },
+              {
+                category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+                threshold: "BLOCK_MEDIUM_AND_ABOVE",
+              },
+            ],
+          }),
+        }
+      )
+        .then((res) => res.json() as Promise<{ candidates: { content: { parts: { text: string }[] } }[] }>)
+        .then((data) => data.candidates[0].content.parts[0].text);
+
+      return response;
+  } catch (error) {
+    return translate[lang].errors[400]
+  }
   }
 
   return (
